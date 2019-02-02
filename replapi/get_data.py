465b83f33561e6db7d64c404c04a20ab0818b49f
Christopher Sardegna <github@reagentx.net>
2019-02-01 23:48:58 -0800

    Get api class working

--- replapi/get_data.py
+++ replapi/get_data.py
@@ -1,4 +1,5 @@
 import re
+import json
 import requests
 import requests_cache
 from datetime import timedelta
@@ -6,7 +7,7 @@ from api_data import API_ROOT, USER_ROOT
 
 
 # Setup cache for requests
-requests_cache.install_cache(expire_after=timedelta(hours=24))
+# requests_cache.install_cache(expire_after=timedelta(hours=24))
 
 
 class ReplIt():
@@ -17,6 +18,7 @@ class ReplIt():
 
 
     def setup(self):
+        '''Get the initial data from the '''
         homepage_user_data = requests.get(f'{USER_ROOT}{self.username}')
         repls = re.findall(f'/@{self.username}/.*?\"', homepage_user_data.text)
         print(repls, len(repls))
@@ -25,20 +27,26 @@ class ReplIt():
         if len(repls) < 20:
             return repls
         else:
-            next_repls = 
+            next_repls = requests.post(f'{API_ROOT}', 
+                                        json=self.create_data(), headers=self.create_headers())
+            print(next_repls.json())
 
 
-    def create_headers(self):
-        {
+    def create_headers(self) -> dict:
+        '''Create headers for the request'''
+        return {'referrer': f'{USER_ROOT}{self.username}'}
+
+
+    def create_data(self) -> dict:
+        return {
             "operationName": "userByUsername",
             "variables": {
-                "username": f"{self.username}",
-                "pinnedReplsFirst": true
+                "username": self.username,
+                "pinnedReplsFirst": 'true'
                 # "after": "MjAxOS0wMS0zMFQyMjo1Mzo0NC4zMTha"
             },
-            "query": "query userByUsername($username: String!, $pinnedReplsFirst: Boolean, $count: Int, $after: String, $before: String, $direction: String, $order: String) {\n  user: userByUsername(username: $username) {\n    id\n    username\n    firstName\n    displayName\n    isLoggedIn\n    repls: publicRepls(pinnedReplsFirst: $pinnedReplsFirst, count: $count, after: $after, before: $before, direction: $direction, order: $order) {\n      items {\n        id\n        timeCreated\n        pinnedToProfile\n        ...ProfileReplItemRepl\n        __typename\n      }\n      pageInfo {\n        hasNextPage\n        nextCursor\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment ProfileReplItemRepl on Repl {\n  id\n  ...ReplItemBaseRepl\n  __typename\n}\n\nfragment ReplItemBaseRepl on Repl {\n  id\n  url\n  title\n  languageDisplayName\n  timeCreated\n  __typename\n}\n"
+            "query": '''query userByUsername($username: String!, $pinnedReplsFirst: Boolean, $count: Int, $after: String, $before: String, $direction: String, $order: String) {\n  user: userByUsername(username: $username) {\n    id\n    username\n    firstName\n    displayName\n    isLoggedIn\n    repls: publicRepls(pinnedReplsFirst: $pinnedReplsFirst, count: $count, after: $after, before: $before, direction: $direction, order: $order) {\n      items {\n        id\n        timeCreated\n        pinnedToProfile\n        ...ProfileReplItemRepl\n        __typename\n      }\n      pageInfo {\n        hasNextPage\n        nextCursor\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment ProfileReplItemRepl on Repl {\n  id\n  ...ReplItemBaseRepl\n  __typename\n}\n\nfragment ReplItemBaseRepl on Repl {\n  id\n  url\n  title\n  languageDisplayName\n  timeCreated\n  __typename\n}\n'''
         }
-
 
 
 r = ReplIt('reagentx')
