f6be5c5f482fba777ed997efe3c5749670b9ae0d
Christopher Sardegna <github@reagentx.net>
2019-02-01 23:58:06 -0800

    Get all of the data from just the graphql endpoint

--- replapi/get_data.py
+++ replapi/get_data.py
@@ -7,7 +7,7 @@ from api_data import API_ROOT, USER_ROOT
 
 
 # Setup cache for requests
-# requests_cache.install_cache(expire_after=timedelta(hours=24))
+requests_cache.install_cache(expire_after=timedelta(hours=24))
 
 
 class ReplIt():
@@ -17,19 +17,17 @@ class ReplIt():
         self.data = self.setup()
 
 
-    def setup(self):
-        '''Get the initial data from the '''
-        homepage_user_data = requests.get(f'{USER_ROOT}{self.username}')
-        repls = re.findall(f'/@{self.username}/.*?\"', homepage_user_data.text)
-        print(repls, len(repls))
-        if len(repls) > 20:
-            raise ValueError(f'Too many regex matches, got {len(repls)}, expected 20.')
-        if len(repls) < 20:
-            return repls
-        else:
-            next_repls = requests.post(f'{API_ROOT}', 
-                                        json=self.create_data(), headers=self.create_headers())
-            print(next_repls.json())
+    def setup(self) -> list:
+        '''Get the initial repl data from the GraphQL API'''
+        repls = requests.post(f'{API_ROOT}', 
+                                    json=self.create_data(), headers=self.create_headers())
+        this_data = json.loads(repls.text)
+        return [item for item in this_data['data']['user']['repls']['items']]
+
+    
+    def get_urls(self) -> list:
+        '''Returns only the urls in the JSON data we collect''' 
+        return [item['url'] for item in self.data]
 
 
     def create_headers(self) -> dict:
@@ -42,7 +40,8 @@ class ReplIt():
             "operationName": "userByUsername",
             "variables": {
                 "username": self.username,
-                "pinnedReplsFirst": 'true'
+                "pinnedReplsFirst": 'true',
+                "count": 9999
                 # "after": "MjAxOS0wMS0zMFQyMjo1Mzo0NC4zMTha"
             },
             "query": '''query userByUsername($username: String!, $pinnedReplsFirst: Boolean, $count: Int, $after: String, $before: String, $direction: String, $order: String) {\n  user: userByUsername(username: $username) {\n    id\n    username\n    firstName\n    displayName\n    isLoggedIn\n    repls: publicRepls(pinnedReplsFirst: $pinnedReplsFirst, count: $count, after: $after, before: $before, direction: $direction, order: $order) {\n      items {\n        id\n        timeCreated\n        pinnedToProfile\n        ...ProfileReplItemRepl\n        __typename\n      }\n      pageInfo {\n        hasNextPage\n        nextCursor\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment ProfileReplItemRepl on Repl {\n  id\n  ...ReplItemBaseRepl\n  __typename\n}\n\nfragment ReplItemBaseRepl on Repl {\n  id\n  url\n  title\n  languageDisplayName\n  timeCreated\n  __typename\n}\n'''
@@ -50,3 +49,5 @@ class ReplIt():
 
 
 r = ReplIt('reagentx')
+print(r.data)
+# print(r.get_urls())
