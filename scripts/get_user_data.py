cb3894d46a5e925499ac78aa9ac6aa0ea4fde64a
Christopher Sardegna <github@reagentx.net>
2019-02-03 22:36:50 -0800

    Update get_user_data.py

--- scripts/get_user_data.py
+++ scripts/get_user_data.py
@@ -4,5 +4,4 @@ from replapi import replit
 repl = replit.ReplIt('reagentx')
 urls = repl.get_urls()
 print(f'Found {len(urls)} repls!')
-# repl.download_all()
-repl.download_zip(repl.get_urls()[0])
+repl.download_all()
