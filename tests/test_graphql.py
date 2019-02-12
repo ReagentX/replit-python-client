18e174e82f1edc32a8328a4f4d52d5f48c5c4f9d
Christopher Sardegna <github@reagentx.net>
2019-02-11 21:18:09 -0800

    rename module

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -1,5 +1,5 @@
 import unittest
-from replapi import get_data
+from replapi import replit
 
 
 class TestJSONMethods(unittest.TestCase):
@@ -7,20 +7,20 @@ class TestJSONMethods(unittest.TestCase):
 
     def test_setup_replit_class(self):
         '''Tests that we create an instance of the class without raising any exceptions'''
-        r = get_data.ReplIt('reagentx', 10)
+        r = replit.ReplIt('reagentx', 10)
         return True
 
 
 
     def test_query(self):
         '''Tests that the query accesses the proper variables'''
-        r = get_data.ReplIt('reagentx', number_to_retreive=10)
+        r = replit.ReplIt('reagentx', number_to_retreive=10)
         self.assertEqual(len(r.data), 10)
 
 
     def test_get_urls(self):
         '''Tests that the get_urls() method returns the proper number of URLS and that they match the expected form'''
-        r = get_data.ReplIt('reagentx', number_to_retreive=10)
+        r = replit.ReplIt('reagentx', number_to_retreive=10)
         urls = r.get_urls()
         self.assertEqual(len(urls), 10)
         for url in urls:
